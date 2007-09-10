aee4a70650a1ff362c91d78a14db3bdccdf5d6eb
apetresc <apetresc@28f6e7a9-5528-0410-be0f-1199edc0a802>
2007-09-10 00:54:14 +0000

    Added a blank config.php file (all sensitive data removed)

---
+++ shogiserver/www/config.php
@@ -0,0 +1,10 @@
+<?php
+	$dbHost = "";	// Host address of database server. Usually "localhost".
+	$dbPort = "";	// Port of database server. Usually 3306.
+	$dbName = "";	// Name of the database to save registrations to.
+	$dbUser = "";	// MySQL user to connect as.
+	$dbPass = "";	// Password for MySQL user
+
+	$dbShogiUserTable = "Users";	// Table in the database containing
+					// shogiserver user records.
+?>
