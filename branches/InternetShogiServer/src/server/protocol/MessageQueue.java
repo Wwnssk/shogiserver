a311b7ee964ff38dbb876b09f01ff5fe696b9c1c
apetresc <apetresc@28f6e7a9-5528-0410-be0f-1199edc0a802>
2008-01-18 21:24:35 +0000

    Added a priority field, accessors, and MAX_PRIORITY field to the MessageQueue.java

--- branches/InternetShogiServer/src/server/protocol/MessageQueue.java
+++ branches/InternetShogiServer/src/server/protocol/MessageQueue.java
@@ -17,6 +17,14 @@ import java.util.Queue;
 public abstract class MessageQueue {
 	
 	protected Queue<ProtocolMessage> messageQueue;
+	protected int priority;
+	
+	/**
+	 * The largest priority a MessageQueue can have on the server. If a
+	 * MessageQueue's priority is set to a value higher than MAX_PRIORITY,
+	 * it will default to MAX_PRIORITY.
+	 */
+	public static final int MAX_PRIORITY = 3;
 	
 	/**
 	 * Returns <code>true</code> if the MessageQueue has no more elements.
@@ -44,5 +52,37 @@ public abstract class MessageQueue {
 	 */
 	public ProtocolMessage dequeue() {
 		return messageQueue.poll();
+	}
+
+	/**
+	 * Sets the priority of this Message Queue. The priority of a MessageQueue 
+	 * is an integer between 0 and <code>MessageQueue.MAX_PRIORITY</code>. The 
+	 * higher the priority value, the sooner the MessageQueue will be processed
+	 * by the main server. It is guaranteed that no message of priority 
+	 * <code>n</code> will be processed until all messages of priority <code>n+1
+	 * </code> have already been processed.
+	 * 
+	 * If a MessageQueue's priority is set to a value higher than MAX_PRIORITY,
+	 * it will default to MAX_PRIORITY. If a MessageQueue's priority is set to
+	 * a value less than 0, it will default to 0.
+	 * 
+	 * @param priority The new priority of this MessageQueue.
+	 */
+	protected void setPriority(int priority) {
+		this.priority = Math.min(Math.max(priority, 0), MAX_PRIORITY);
+	}
+
+	/**
+	 * Gets the priority of this Message Queue. The priority of a MessageQueue 
+	 * is an integer between 0 and <code>MessageQueue.MAX_PRIORITY</code>. The 
+	 * higher the priority value, the sooner the MessageQueue will be processed
+	 * by the main server. It is guaranteed that no message of priority 
+	 * <code>n</code> will be processed until all messages of priority <code>n+1
+	 * </code> have already been processed.
+	 * 
+	 * @return The priority of this MessageQueue.
+	 */
+	protected int getPriority() {
+		return priority;
 	}
 }
