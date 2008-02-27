6808bfae7e888212f1136aa10aa892cfa8813ee1
apetresc <apetresc@28f6e7a9-5528-0410-be0f-1199edc0a802>
2008-02-27 18:33:26 +0000

    Fleshing out the SGF parser a bit.

--- SGFParser/src/org/sgf/parser/GameTree.java
+++ SGFParser/src/org/sgf/parser/GameTree.java
@@ -1,5 +1,27 @@
 package org.sgf.parser;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class GameTree {
 
+	private Sequence sequence;
+	private List<GameTree> gameTrees;
+	
+	public GameTree() {
+		sequence = new Sequence();
+		gameTrees = new LinkedList<GameTree>();
+	}
+	
+	public String toString() {
+		String result = "(" + sequence.toString();
+		Iterator<GameTree> i = gameTrees.iterator();
+		while (i.hasNext()) {
+			result += i.next().toString();
+		}
+		result += ")";
+			
+		return result;
+	}
 }
---
+++ SGFParser/src/org/sgf/parser/test/ParserTest.java
@@ -0,0 +1,14 @@
+package org.sgf.parser.test;
+
+public class ParserTest {
+	
+	public boolean testSimpleParse() {
+		
+		return false;
+	}
+	
+	public boolean testSimpleGenerate() {
+		
+		return false;
+	}
+}
