100ce90918956b834471c86c672fd8d31bcaffd1
apetresc <apetresc@28f6e7a9-5528-0410-be0f-1199edc0a802>
2008-04-06 14:58:23 +0000

    Getting rid of old parser to make way for a new one.

--- trunk/SGFParser/src/org/sgf/parser/go/GoGameTree.java
+++
@@ -1,7 +0,0 @@
-package org.sgf.parser.go;
-
-import org.sgf.parser.GameTree;
-
-public class GoGameTree extends GameTree {
-
-}
--- trunk/SGFParser/src/org/sgf/parser/go/GoParser.java
+++
@@ -1,7 +0,0 @@
-package org.sgf.parser.go;
-
-import org.sgf.parser.Parser;
-
-public class GoParser extends Parser {
-
-}
