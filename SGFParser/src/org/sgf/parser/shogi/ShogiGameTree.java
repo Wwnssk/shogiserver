7b6c7b3391ee8ed155f3903552eb4d6195689656
apetresc <apetresc@28f6e7a9-5528-0410-be0f-1199edc0a802>
2008-04-06 14:58:23 +0000

    Getting rid of old parser to make way for a new one.

--- SGFParser/src/org/sgf/parser/shogi/ShogiGameTree.java
+++
@@ -1,7 +0,0 @@
-package org.sgf.parser.shogi;
-
-import org.sgf.parser.GameTree;
-
-public class ShogiGameTree extends GameTree {
-
-}
--- SGFParser/src/org/sgf/parser/shogi/ShogiParser.java
+++
@@ -1,7 +0,0 @@
-package org.sgf.parser.shogi;
-
-import org.sgf.parser.Parser;
-
-public class ShogiParser extends Parser {
-
-}
